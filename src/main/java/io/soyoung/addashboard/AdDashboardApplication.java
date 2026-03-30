ecdb3c6b0bd6881546e48380da0e2665110eb949
Soyoung Lee <soo7132@naver.com>
2026-03-30 14:26:32 +0900

    Initial Commit: 프로젝트 환경 설정

---
+++ src/main/java/io/soyoung/addashboard/AdDashboardApplication.java
@@ -0,0 +1,13 @@
+package io.soyoung.addashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/soyoung/addashboard/AdDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package io.soyoung.addashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
