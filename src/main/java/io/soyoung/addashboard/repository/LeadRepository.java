28995a1f864a5a09253fd80a36f1b5871c8497f7
Soyoung Lee <soo7132@naver.com>
2026-03-31 11:21:53 +0900

    Feat: User, Lead 레포지토리 메서드 추가 #8

--- src/main/java/io/soyoung/addashboard/repository/LeadRepository.java
+++ src/main/java/io/soyoung/addashboard/repository/LeadRepository.java
@@ -1,8 +1,15 @@
 package io.soyoung.addashboard.repository;
 
 import io.soyoung.addashboard.entity.Lead;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LeadRepository extends JpaRepository<Lead, Long> {
 
+    Optional<Lead> findByEmail(String email);
+
+    List<Lead> findAllByEmail(String email);
+
+    long countByMetaCampaignId(String metaCampaignId);
 }
